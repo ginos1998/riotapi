4682c901dd43aed47ab3fab06bc09968c56dab0f
ginos1998 <ginosiam1998@gmail.com>
2023-07-25 19:50:33 -0300

    fix

--- src/main/java/riot/riotapi/configuration/FactoryConfig.java
+++ src/main/java/riot/riotapi/configuration/FactoryConfig.java
@@ -4,33 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import riot.riotapi.repositories.factories.PersistenceFactory;
 import riot.riotapi.repositories.interfaces.*;
-import riot.riotapi.services.ServiceFactory;
-import riot.riotapi.services.interfaces.IntChampionApiService;
-import riot.riotapi.services.interfaces.IntSummonerApiService;
-import riot.riotapi.services.interfaces.IntSummonerService;
 
 @Configuration
 public class FactoryConfig {
 
   public FactoryConfig(){
     // default
-  }
-
-  //***************************** SERVICE FACTORY *****************************//
-
-  @Autowired
-  public void setIntSummonerService(IntSummonerService intSummonerService) {
-    ServiceFactory.setIntSummonerService(intSummonerService);
-  }
-
-  @Autowired
-  public void setIntSummonerApiService(IntSummonerApiService intSummonerApiService) {
-    ServiceFactory.setIntSummonerApiService(intSummonerApiService);
-  }
-
-  @Autowired
-  public void setIntChampionApiService(IntChampionApiService intChampionApiService) {
-    ServiceFactory.setIntChampionApiService(intChampionApiService);
   }
 
   //***************************** PERSISTENCE FACTORY *****************************//
