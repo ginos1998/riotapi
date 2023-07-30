4b0646e3035c9ab73789fe36d67efa4e5e2be41b
ginos1998 <ginosiam1998@gmail.com>
2023-07-30 18:52:14 -0300

    tests unitarios actualizados para SummonerDelegator

--- src/test/java/riot/riotapi/delegators/SummonerDelegatorTest.java
+++ src/test/java/riot/riotapi/delegators/SummonerDelegatorTest.java
@@ -6,9 +6,16 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import riot.riotapi.dtos.SummonerDTO;
+import riot.riotapi.entities.Summoner;
 import riot.riotapi.services.implementations.ImpSummonerApiService;
+import riot.riotapi.services.interfaces.IntSummonerService;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 
@@ -18,6 +25,8 @@ public class SummonerDelegatorTest {
 
   @Mock
   private ImpSummonerApiService summonerApiServiceMock;
+  @Mock
+  private IntSummonerService summonerServiceMock;
 
   @InjectMocks
   private SummonerDelegador summonerDelegador;
@@ -27,31 +36,77 @@ public class SummonerDelegatorTest {
     MockitoAnnotations.openMocks(this);
   }
 
+  /**
+   * Verifies if, given a 'name' (not null or empty), the app search a summoner in the database
+   * and return a not empty list. It means the summoner exists in the database.
+   */
   @Test
-  public void getSummonerByName_PositiveTest() {
+  public void getSummonerByName_SearchBd_PositiveTest() {
     // given
     String id = "123456789";
     String accountId = "987654321";
     String puuid = "135798642";
     String name = "pepito";
     SummonerDTO expectedSummoner = new SummonerDTO(id, accountId, puuid, name, 0, 0, 0);
-
+    List<SummonerDTO> expectedList = new ArrayList<>(Collections.singletonList(expectedSummoner));
     String expectedName = "pepito";
 
     // then
-    when(summonerApiServiceMock.getSummonerByName(expectedName))
-        .thenReturn(expectedSummoner);
+    when(summonerServiceMock.getSummonerByName(expectedName))
+        .thenReturn(expectedList);
 
     // when
-    SummonerDTO receivedSumDTO = summonerDelegador.getSummonerByName(expectedName);
+    List<SummonerDTO> receivedList = summonerDelegador.getSummonerBy(expectedName, null, null, true);
 
-    verify(summonerApiServiceMock, times(1)).getSummonerByName(expectedName);
-    Assert.assertNotNull(receivedSumDTO);
-    Assert.assertEquals(receivedSumDTO, expectedSummoner);
+    verify(summonerApiServiceMock, times(0)).getSummonerByName(expectedName);
+    verify(summonerServiceMock, times(1)).getSummonerByName(expectedName);
+    Assert.assertNotNull(receivedList);
+    Assert.assertEquals(receivedList, expectedList);
+
   }
 
+  /**
+   * Verifies if, given a 'name' (not null or empty), the app search a summoner first in the database
+   * and if the returned list is null or empty, then search the summoner creating a request to the api.
+   * Finally, also verifies if the returned list is not null.
+   */
   @Test
-  public void getSummonerByPuuid_PositiveTest() {
+  public void getSummonerByName_SearchApi_PositiveTest() {
+    // given
+    String id = "123456789";
+    String accountId = "987654321";
+    String puuid = "135798642";
+    String name = "pepito";
+    SummonerDTO expectedSummoner = new SummonerDTO(id, accountId, puuid, name, 0, 0, 0);
+    List<SummonerDTO> expectedList = new ArrayList<>(Collections.singletonList(expectedSummoner));
+    String expectedName = "pepito";
+    ModelMapper mapper = new ModelMapper();
+
+    // then
+    when(summonerServiceMock.getSummonerByName(expectedName))
+        .thenReturn(new ArrayList<>());
+
+    when(summonerApiServiceMock.getSummonerByName(expectedName))
+        .thenReturn(expectedList);
+
+    // when
+    List<SummonerDTO> receivedList = summonerDelegador.getSummonerBy(expectedName, null, null, true);
+
+    verify(summonerApiServiceMock, times(1)).getSummonerByName(expectedName);
+    verify(summonerServiceMock, times(1)).getSummonerByName(expectedName);
+    verify(summonerServiceMock, times(1)).saveSummoner(mapper.map(receivedList.get(0), Summoner.class));
+    Assert.assertNotNull(receivedList);
+    Assert.assertEquals(receivedList, expectedList);
+
+  }
+
+  /**
+   * Verifies if, given a 'puuid' (not null or empty), the app search a summoner first in the database
+   * and if the returned list is null or empty, then search the summoner creating a request to the api.
+   * Finally, also verifies if the returned list is not null.
+   */
+  @Test
+  public void getSummonerByPuuid_SearchApi_PositiveTest() {
     // given
     String id = "123456789";
     String accountId = "987654321";
@@ -60,21 +115,31 @@ public class SummonerDelegatorTest {
     SummonerDTO expectedSummoner = new SummonerDTO(id, accountId, puuid, name, 0, 0, 0);
 
     String expectedPuuid = "135798642";
+    List<SummonerDTO> expectedList = new ArrayList<>(Collections.singletonList(expectedSummoner));
+    ModelMapper mapper = new ModelMapper();
 
     // then
+    when(summonerServiceMock.getSummonerByPuuid(expectedPuuid))
+        .thenReturn(new ArrayList<>());
     when(summonerApiServiceMock.getSummonerByPuuid(expectedPuuid))
-        .thenReturn(expectedSummoner);
+        .thenReturn(expectedList);
 
     // when
-    SummonerDTO receivedSumDTO = summonerDelegador.getSummonerByPuuid(expectedPuuid);
+    List<SummonerDTO> receivedList = summonerDelegador.getSummonerBy(null, null, expectedPuuid, true);
 
+    verify(summonerServiceMock, times(1)).getSummonerByPuuid(expectedPuuid);
     verify(summonerApiServiceMock, times(1)).getSummonerByPuuid(expectedPuuid);
-    Assert.assertNotNull(receivedSumDTO);
-    Assert.assertEquals(receivedSumDTO, expectedSummoner);
+    verify(summonerApiServiceMock, times(1)).getSummonerByPuuid(expectedPuuid);
+    Assert.assertNotNull(receivedList);
+    Assert.assertEquals(receivedList, expectedList);
   }
 
+  /**
+   * Verifies if, given a 'puuid' (not null or empty), the app search a summoner in the database
+   * and return a not empty list. It means the summoner exists in the database.
+   */
   @Test
-  public void getSummonerByAccountId_PositiveTest() {
+  public void getSummonerByPuuid_SearchBd_PositiveTest() {
     // given
     String id = "123456789";
     String accountId = "987654321";
@@ -82,18 +147,87 @@ public class SummonerDelegatorTest {
     String name = "pepito";
     SummonerDTO expectedSummoner = new SummonerDTO(id, accountId, puuid, name, 0, 0, 0);
 
+    String expectedPuuid = "135798642";
+    List<SummonerDTO> expectedList = new ArrayList<>(Collections.singletonList(expectedSummoner));
+    ModelMapper mapper = new ModelMapper();
+
+    // then
+
+    when(summonerServiceMock.getSummonerByPuuid(expectedPuuid))
+        .thenReturn(expectedList);
+
+    // when
+    List<SummonerDTO> receivedList = summonerDelegador.getSummonerBy(null, null, expectedPuuid, true);
+
+    verify(summonerServiceMock, times(1)).getSummonerByPuuid(expectedPuuid);
+    verify(summonerApiServiceMock, times(0)).getSummonerByPuuid(expectedPuuid);
+    verify(summonerApiServiceMock, times(0)).getSummonerByPuuid(expectedPuuid);
+    Assert.assertNotNull(receivedList);
+    Assert.assertEquals(receivedList, expectedList);
+  }
+
+  /**
+   * Verifies if, given a 'accountId' (not null or empty), the app search a summoner in the database
+   * and return a not empty list. It means the summoner exists in the database.
+   */
+  @Test
+  public void getSummonerByAccountId_SearchBd_PositiveTest() {
+    // given
+    String id = "123456789";
+    String accountId = "987654321";
+    String puuid = "135798642";
+    String name = "pepito";
+    SummonerDTO expectedSummoner = new SummonerDTO(id, accountId, puuid, name, 0, 0, 0);
+    List<SummonerDTO> expectedList = new ArrayList<>(Collections.singletonList(expectedSummoner));
+    ModelMapper mapper = new ModelMapper();
     String expectedAccountId = "987654321";
 
     // then
-    when(summonerApiServiceMock.getSummonerByAccountId(expectedAccountId))
-        .thenReturn(expectedSummoner);
+    when(summonerServiceMock.getSummonerByAccountId(expectedAccountId))
+        .thenReturn(expectedList);
 
     // when
-    SummonerDTO receivedSumDTO = summonerDelegador.getSummonerByAccountId(expectedAccountId);
+    List<SummonerDTO> receivedList = summonerDelegador.getSummonerBy(null, expectedAccountId, null, true);
 
+    verify(summonerServiceMock, times(1)).getSummonerByAccountId(expectedAccountId);
+    verify(summonerApiServiceMock, times(0)).getSummonerByAccountId(expectedAccountId);
+    verify(summonerApiServiceMock, times(0)).getSummonerByAccountId(expectedAccountId);
+    Assert.assertNotNull(receivedList);
+    Assert.assertEquals(receivedList, expectedList);
+  }
+
+  /**
+   * Verifies if, given a 'accountId' (not null or empty), the app search a summoner first in the database
+   * and if the returned list is null or empty, then search the summoner creating a request to the api.
+   * Finally, also verifies if the returned list is not null.
+   */
+  @Test
+  public void getSummonerByAccountId_SearchApi_PositiveTest() {
+    // given
+    String id = "123456789";
+    String accountId = "987654321";
+    String puuid = "135798642";
+    String name = "pepito";
+    SummonerDTO expectedSummoner = new SummonerDTO(id, accountId, puuid, name, 0, 0, 0);
+    List<SummonerDTO> expectedList = new ArrayList<>(Collections.singletonList(expectedSummoner));
+    ModelMapper mapper = new ModelMapper();
+    String expectedAccountId = "987654321";
+
+    // then
+    when(summonerServiceMock.getSummonerByAccountId(expectedAccountId))
+        .thenReturn(new ArrayList<>());
+
+    when(summonerApiServiceMock.getSummonerByAccountId(expectedAccountId))
+        .thenReturn(expectedList);
+
+    // when
+    List<SummonerDTO> receivedList = summonerDelegador.getSummonerBy(null, expectedAccountId, null, true);
+
+    verify(summonerServiceMock, times(1)).getSummonerByAccountId(expectedAccountId);
     verify(summonerApiServiceMock, times(1)).getSummonerByAccountId(expectedAccountId);
-    Assert.assertNotNull(receivedSumDTO);
-    Assert.assertEquals(receivedSumDTO, expectedSummoner);
+    verify(summonerApiServiceMock, times(1)).getSummonerByAccountId(expectedAccountId);
+    Assert.assertNotNull(receivedList);
+    Assert.assertEquals(receivedList, expectedList);
   }
 
 }
