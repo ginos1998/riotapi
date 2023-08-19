d497cd02303b9ae5dc1d252b0800d98789ab6f52
ginos1998 <ginosiam1998@gmail.com>
2023-08-18 23:22:02 -0300

    jda imp

---
+++ MESSAGES.md
@@ -0,0 +1,21 @@
+> **bold text in a quote**.
+>
+> | Syntax      | Description | Test Text     |
+> | :---        |    :----:   |          ---: |
+> | Header      | Title       | Here's this   |
+> | Paragraph   | Text        | And more      |
+
+
+> Esto es una cita con una tabla:
+>
+> | Columna 1 | Columna 2 |
+> |-----------|-----------|
+> | Celda 1   | Celda 2   |
+> | Celda 3   | Celda 4   |
+>
+> Y aquí continúa la cita.
+
+:soccer: 
+
+<h3 id="custom-id">My Great Heading</h3>
+
--- src/main/java/riot/riotapi/externals/discord/configuration/BotConfiguration.java
+++ src/main/java/riot/riotapi/externals/discord/configuration/BotConfiguration.java
@@ -3,40 +3,61 @@ package riot.riotapi.externals.discord.configuration;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.Event;
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import riot.riotapi.exceptions.DiscordException;
+import riot.riotapi.externals.discord.listeners.BotListener;
 
-import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Configuration
 public class BotConfiguration {
 
     @Value("${discord.token}")
     private String token;
+    private JDA jda;
+    Logger logger = LoggerFactory.getLogger(BotConfiguration.class);
 
     @Bean
-    public <T extends Event> GatewayDiscordClient gatewayDiscordClient(List<EventListener<T>> eventListeners) {
-        GatewayDiscordClient client;
+    public JDA jda() {
         try {
-             client = DiscordClientBuilder.create(token)
-                    .build()
-                    .login()
-                    .block();
-
-            for(EventListener<T> listener : eventListeners) {
-                assert client != null;
-                client.on(listener.getEventType())
-                        .flatMap(listener::execute)
-                        .onErrorResume(listener::handleError)
-                        .subscribe();
-            }
+            return JDABuilder.createLight(token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS)
+                .addEventListeners(new BotListener())
+                .build();
         } catch (BeanCreationException ex) {
             throw new DiscordException("ERROR al inicializar discord.\n".concat(ex.getMessage()), ex);
+        } finally {
+            logger.info("Discord BOT has started successfully!");
         }
-
-        return client;
     }
+//    @Bean
+//    public <T extends Event> GatewayDiscordClient gatewayDiscordClient(List<EventListener<T>> eventListeners) {
+//        GatewayDiscordClient client;
+//        try {
+//             client = DiscordClientBuilder.create(token)
+//                    .build()
+//                    .login()
+//                    .block();
+//
+//            for(EventListener<T> listener : eventListeners) {
+//                assert client != null;
+//                client.on(listener.getEventType())
+//                        .flatMap(listener::execute)
+//                        .onErrorResume(listener::handleError)
+//                        .subscribe();
+//            }
+//        } catch (BeanCreationException ex) {
+//            throw new DiscordException("ERROR al inicializar discord.\n".concat(ex.getMessage()), ex);
+//        }
+//
+//        return client;
+//    }
 }
